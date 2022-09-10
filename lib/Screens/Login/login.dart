a5be4aeeb741572fc48039d1446805e8dcba1a46
Hammad khan <hammad.khan0312@gmail.com>
2022-09-10 13:43:39 +0500

    first commit

---
+++ lib/Screens/Login/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset('assets/logo.jpeg')],
+        ),
+      ),
+    );
+  }
+}
